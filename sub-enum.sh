2343de3dce27e8365c603c9858dc0be5a7f00b1f
Andrew Abletsov <abletsoff@gmail.com>
2023-04-17 23:00:28 +0300

    Add Google search enumeration

--- sub-enum.sh
+++ sub-enum.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 f_transparency () {
-	unset subs
 	json_data=$(curl "https://crt.sh/?q={$domain}&output=json" 2>/dev/null)
 	
 	# Retrieving CN certificate field 
@@ -16,113 +15,60 @@ f_transparency () {
 	transparency_subs=($(for sub in "${tmp_names[@]}"; \
        		do echo "${sub}"; done | sort -u))
 
+	echo "--- Certificate transparency ---"	
 	for sub in ${transparency_subs[@]}; do
 		if [ "$sub" != "$domain" ] && [ "$sub" != "*.$domain" ]; then
-			f_related_decision $sub
+		       	if [[ "$sub" == *"$domain"* ]]; then
+				echo $sub
+			else
+				related=(${related[@]} "$sub")
+			fi
 		fi
 	done
-	f_output "ctr.sh" "${subs[@]}" 
 }
 
 f_spf () {
-	unset subs
 	spf_record=$(dig TXT +short $domain | grep -P '^"v=spf1')
-	spf_subs=$(echo $spf_record | grep -o -P "(a|include):\S*|redirect=\S*" \
-		| cut -d ":" -f2 | cut -d "=" -f2 | cut -d '"' -f1| sort -u)
+	spf_a=$(echo $spf_record | grep -o "a:\S*" | cut -d ":" -f2)
+	
+	# include:
+	# redirect:
+	# ip4: (PTR enumeration)
 
-	for sub in ${spf_subs[@]}; do
+	echo "--- SPF ---"	
+	for sub in $spf_a; do
 		if [ "$sub" != "$domain" ]; then
-			f_related_decision $sub
+			echo $sub
 		fi
 	done
-	f_output "SPF" "${subs[@]}"	
 }
 
-f_mx () {
-	mx_record="$(dig MX +short $domain)"
-	mx_subs=$(echo $mx_record | grep -o -P "\d \S*." |  cut -d " " -f2 | sed "s/.$//g")
-	for sub in ${mx_subs[@]}; do
-		f_related_decision $sub
+f_tmp_related () {
+	echo "--- Related ---"
+	for sub in ${related[@]}; do
+		echo $sub
 	done
-	f_output "MX" "${subs[@]}"	
 
-	
-}
-
-f_google () {
-	# site:*.example.com -inurl:www.example.com
 }
 
 f_print_help () {
 	echo -e "Usage: sub-enum [options...] <domain>\n" \
-		"-h\tdisplay this help and exit\n" \
-		"-m\tMX entry analyzing\n" \
-		"-s\tSPF entry analyzing\n" \
-		"-t\tCertificate transparancy check (crt.sh)\n" \
-		"-O\tMarkdown output\n" \
-		"-T\tTruncated output"
-}
-
-f_related_decision () {
-	sub=$1
-	if [[ "$sub" == *"$domain"* ]]; then
-		subs=(${subs[@]} "$sub")
-	else
-		related=(${related[@]} "$sub")
-	fi
+			 "-h\tdisplay this help and exit\n" \
+			 "-s\tSPF entry analyzing\n" \
+			 "-t\tCertificate transparancy check (crt.sh)\n"
+			 "-o\tMarkdown output"
 }
 
 f_output () {
-	description=$1
-	shift 
-	subdomains=("$@")
-
-	for sub in ${subdomains[@]}; do
-
-		# Domain address resolving
-		resolve=$(dig $sub +short | sed -z "s/\n/, /g" | sed "s/, $//g")
-		if [[ $resolve = '' ]]; then
-			resolve="unresolved"
-		fi
-
-		# Avoiding duplicate entries
-		duplicate="false"
-		for p_sub in ${printed_subdomains[@]}; do
-			if [[ $sub = $p_sub ]]; then
-				duplicate="true"
-				break
-			fi
-		done
 	
-		if [[ $duplicate = "true" ]]; then
-			continue
-		else	
-			printed_subdomains=(${printed_subdomains[@]} "$sub")
-		fi
-		
-		# Output
-		if [[ $truncated_output = "true" ]]; then
-			sub=$(echo $sub | sed "s/\.$domain$//g")
-		fi	
-		if [[ $markdown_output = "true" ]]; then
 
-			echo "|$resolve|$sub|$description|"
-		else
-			echo "$sub - $resolve"
-		fi
-	done
 }
 
-while getopts "hmstOT" opt; do
+while getopts "hst" opt; do
 	case $opt in
-		m)	mx_check="true"
-			check="true";;
-		s)	spf_check="true"
-			check="true";;
-		t)	transparency_check="true"
-			check="true";;
-		O)	markdown_output="true";;
-		T)	truncated_output="true";;
+		s)	spf_check="true";;
+		t)	transparency_check="true";;
+		o)	markdown_output="true";;
 		h) 	f_print_help
 			exit;;
 		?) 	exit;;
@@ -133,26 +79,13 @@ shift $((OPTIND-1))
 domain=$1
 
 if [[ $domain != "" ]]; then
-	if [[ $markdown_output = "true" ]]; then
-		echo "|Resolve|Domain|Source|"
-		echo "|---|---|---|"
-	fi
-	if [[ $mx_check = "true" ]]; then
-		f_mx
-	fi
 	if [[ $spf_check = "true" ]]; then
 		f_spf
 	fi
 	if [[ $transparency_check = "true" ]]; then
 		f_transparency
 	fi
-	if [[ $check != "true" ]]; then
-		f_mx
-		f_spf
-		f_transparency
-	fi
-
-	f_output "Related" "${related[@]}" 
+	f_tmp_related
 else
 	f_print_help
 fi

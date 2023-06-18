f99c9c38a84c1bb33d73c22240abfc2c7ad21957
Andrew Abletsov <abletsoff@gmail.com>
2023-06-18 21:55:33 +0300

    CSP header check

--- sub-enum.sh
+++ sub-enum.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-domain_regex="(\w|\.|-|\*)*"
+domain_regex="(_?([a-z0-9-]){1,61}\.)+[a-z0-9]{1,61}"
+user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
 
 f_transparency () {
-	json_data=$(curl "https://crt.sh/?q={$domain}&output=json" 2>/dev/null)
+	json_data=$(curl -A "$user_agent" "https://crt.sh/?q={$domain}&output=json" 2>/dev/null)
 	
 	# Retrieving CN certificate field 
 	common_names=$(echo $json_data | grep -P -o "\"common_name\":\"${domain_regex}\"" | \
@@ -55,10 +56,30 @@ f_zone_transfer () {
 	nameservers=($(dig NS +short ${domain}))
        	for nameserver in ${nameservers[@]}; do	
 		axfr_response=$(dig AXFR ${domain} @${nameserver})
-		subs=$(echo "$axfr_response" | grep -o -P "${domain_regex}${domain}" \
-		| sort -u | uniq)
+		subs=$(echo "$axfr_response" | grep -o -P "${domain_regex}" \
+		| grep ${domain} | sort -u | uniq)
 		f_output "Zone transfer" "${subs[@]}"
 	done
+}
+
+f_ptr_lookup () {
+	ip_addresses=("$@")
+	for ip in ${ip_addresses[@]}; do
+		record=$(dig +short -x $ip | sed "s/\.$//g")
+		subs=(${subs[@]} "$record")
+	done
+	f_output "PTR lookup" "${subs[@]}"
+}
+
+f_csp () {
+	printed_subs=("$@")
+	for sub in ${printed_subs[@]}; do
+		csp_header=$(curl -A "$user_agent" -s -D - -o /dev/null -k -L ${sub} | \
+			grep --ignore-case "^Content-Security-Policy: ")
+		csp_domains=$(echo $csp_header | grep --color -o -P "${domain_regex}")
+		subs=(${subs[@]} "${csp_domains[@]}")
+	done
+	f_output "CSP" "${subs[@]}"
 }
 
 f_print_help () {
@@ -68,6 +89,8 @@ f_print_help () {
 		"-g\tGoogle search\n" \
 		"-t\tCertificate transparancy check (crt.sh)\n" \
 		"-z\tZone transfering\n" \
+		"-p\tPTR lookup\n" \
+		"-c\tContent-Security-Policy analyzing\n" \
 		"-O\tMarkdown output\n" \
 		"-X\tExclude uresolved domains\n" \
 		"-T\tTruncated output"
@@ -80,10 +103,26 @@ f_output () {
 		
 	for sub in ${subs[@]}; do
 		# Domain address resolving
-		resolve=$(dig $sub +short | sed -z "s/\n/, /g" | sed "s/, $//g")
+		#===Need to handle CNAME resolve troubles===
+		resolve=$(dig +short "$sub")
 		if [[ $resolve = '' ]]; then
 			resolve="unresolved"
+		else
+			for resolved_ip in ${resolve[@]}; do
+				duplicate="false"
+				for ip in ${ip_addresses[@]}; do
+					if [[ "$ip" == "$resolved_ip" ]]; then
+						duplicate="true"
+						break
+					fi
+				done
+				if [[ "$duplicate" == "false" ]]; then
+					ip_addresses=(${ip_addresses[@]} "$resolved_ip")
+				fi
+			done
 		fi
+
+		resolve=$(echo "$resolve" | sed -z "s/\n/, /g" | sed "s/, $//g")
 
 		# Avoiding duplicate entries
 		duplicate="false"
@@ -123,7 +162,7 @@ f_output () {
 	done
 }
 
-while getopts "hegtzOXT" opt; do
+while getopts "hegtzpcOXT" opt; do
 	case $opt in
 		e)	email_check="true"
 			check="true";;
@@ -132,6 +171,9 @@ while getopts "hegtzOXT" opt; do
 		t)	transparency_check="true"
 			check="true";;
 		z)	zone_transfer="true"
+			check="true";;
+		p)	ptr_lookup="true";;
+		c)	csp="true"
 			check="true";;
 		O)	markdown_output="true";;
 		X)	exclude_uresolved="true";;
@@ -166,6 +208,12 @@ if [[ $domain != "" ]]; then
 	if [[ $zone_transfer = "true" ]]; then
 		f_zone_transfer
 	fi
+	if [[ $csp = "true" ]]; then
+		f_csp "${printed_subdomains[@]}"
+	fi
+	if [[ $ptr_lookup = "true" ]]; then
+		f_ptr_lookup "${ip_addresses[@]}"
+	fi
 	if [[ $check != "true" ]]; then
 		f_mx
 		f_spf
@@ -173,6 +221,7 @@ if [[ $domain != "" ]]; then
 		f_google
 		f_transparency
 		f_zone_transfer
+		f_ptr_lookup "${ip_addresses[@]}"
 	fi
 
 	if (( ${#related[@]} )); then
@@ -180,8 +229,9 @@ if [[ $domain != "" ]]; then
 		for r_domain in ${related[@]}; do
 			echo -n " $r_domain;"
 		done
-		echo
+		echo ""
 	fi
+
 else
 	f_print_help
 fi
